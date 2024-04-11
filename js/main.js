938fd7a3b1a3cc2e96551bd30fa6aa8c50d07815
BereVelez <ab.jvelez@gmail.com>
2024-04-11 13:51:01 -0600

    Se contruyó la base de las cards de plantas y servicios para recibir la Api

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 //Scroll de NavBar para contraste
 window.addEventListener("scroll", function(){
+
+    //Agrega clases para modificar el nav-bar al deslizar hacia abajo
+    
     var navegador = document.getElementById("navegador");
     navegador.classList.toggle("nav-verde", window.scrollY>300);
+
     var logo = document.getElementById("logo");
     logo.classList.toggle("logo-peque", window.scrollY>300)
+
     var menu = document.getElementById("menu");
     menu.classList.toggle("obscuro", window.scrollY>300)
 })
+
+//Consumo de Api para productos
